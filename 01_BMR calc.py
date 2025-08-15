81b129033773c7ad3a5a8fdb08a739c2b1036fca
Darshan Mahajan <mahajandarshan577@gmail.com>
2025-08-15 22:35:38 +0530

    Add files via upload

---
+++ 01_BMR calc.py
@@ -0,0 +1,23 @@
+gender = input("Enter your gender(M/F): ")
+
+weight = int(input("Enter your weight in kg"))
+
+height = int(input("Enter your height in cm"))
+
+age = int(input("Enter your age in years"))
+
+
+
+if gender == 'M':
+    BMRFM = 66.5 + (13.75 * weight) + (5.003 * height) - (6.75 *age)
+    print("Your") 
+    print(BMRFM)
+
+
+elif gender == 'F':
+    BMRFW = 655.1 + (9.563 *weight) +(1.850 * height) - (4.676 *age)
+    print("Your") 
+    print(BMRFW)
+
+else :
+    print("You are gay")
